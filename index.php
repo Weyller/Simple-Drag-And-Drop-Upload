4b692d41729cd506382de2a8331a4df62d05679d
weyller <weyller@gmail.com>
2017-08-20 01:47:33 -0400

    1st commit

--- index.php
+++ index.php
@@ -10,10 +10,11 @@
     <div id="wrapper">
       <div id="drop-area">
         <h3 class="drop-text">Drag and Drop Images Here</h3>
-        <!-- <img src="no_image.jpg" alt="image" class="image"> -->
+
       </div>
     </div>
-
+    <p></p>
+    <img src="images/17455538fd839328f5606d284d0c360d.jpg" alt="image" class="image">
 
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script type="text/javascript" src="main.js"></script>
